c6ac34ab0951cd2a24a7508cc1bdb2e89af91a17
n-rick <e.ficadiere@gmail.com>
2021-10-10 16:41:52 +0200

    add js file

---
+++ app.js
@@ -0,0 +1,9 @@
+// Elements du DOM
+
+const divVies = document.querySelector('.vies');
+const message = document.getElementById('message');
+const formulaire = document.getElementById('inputBox');
+const input = document.getElementById('number');
+const essayerBtn = document.getElementById('essayerBtn');
+const rejouerBtn = document.getElementById('rejouer');
+const body = document.getElementsByTagName('body')[0];
